4a87f9d445cdd04f8bd51ab6e6c40e44cfe0510f
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-04-27 13:24:58 +0300

    stylings

--- oto-galery/src/views/AboutView.vue
+++ oto-galery/src/views/AboutView.vue
@@ -1,15 +1,101 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="mx-auto max-w-screen-xl">
+    <main class="mt-10">
+      <div class="relative mx-auto mb-4 w-full max-w-screen-md md:mb-0" style="height: 24em">
+        <div
+          class="absolute bottom-0 left-0 z-10 h-full w-full"
+          style="background-image: linear-gradient(180deg, transparent, rgba(0, 0, 0, 0.7))"
+        ></div>
+        <img
+          src="../assets/gabriel-gurrola-u6BPMXgURuI-unsplash.jpg"
+          class="absolute left-0 top-0 z-0 h-full w-full object-cover"
+        />
+        <div class="absolute bottom-0 left-0 z-20 p-4 sm:bottom-4">
+          <h2 class="text-xl font-semibold leading-tight text-gray-100 sm:text-4xl">
+            Pellentesque a consectetur velit, ac molestie ipsum.
+          </h2>
+        </div>
+      </div>
+
+      <div class="mx-auto mt-12 max-w-screen-md px-4 text-lg leading-relaxed text-gray-700 lg:px-0">
+        <p class="pb-6">
+          Advantage old had otherwise sincerity dependent additions. It in adapted natural hastily
+          is justice. Six draw you him full not mean evil. Prepare garrets it expense windows
+          shewing do an. She projection advantages resolution son indulgence. Part sure on no long
+          life am at ever. In songs above he as drawn to. Gay was outlived peculiar rendered led
+          six.
+        </p>
+
+        <p class="pb-6">
+          Difficulty on insensible reasonable in. From as went he they. Preference themselves me as
+          thoroughly partiality considered on in estimating. Middletons acceptance discovered
+          projecting so is so or. In or attachment inquietude remarkably comparison at an. Is
+          surrounded prosperous stimulated am me discretion expression. But truth being state can
+          she china widow. Occasional preference fat remarkably now projecting uncommonly
+          dissimilar. Sentiments projection particular companions interested do at my delightful.
+          Listening newspaper in advantage frankness to concluded unwilling.
+        </p>
+
+        <p class="pb-6">
+          Adieus except say barton put feebly favour him. Entreaties unpleasant sufficient few
+          pianoforte discovered uncommonly ask. Morning cousins amongst in mr weather do neither.
+          Warmth object matter course active law spring six. Pursuit showing tedious unknown winding
+          see had man add. And park eyes too more him. Simple excuse active had son wholly coming
+          number add. Though all excuse ladies rather regard assure yet. If feelings so prospect no
+          as raptures quitting.
+        </p>
+
+        <div class="mb-6 rounded border-l-4 border-gray-500 pl-4 italic">
+          Sportsman do offending supported extremity breakfast by listening. Decisively advantages
+          nor expression unpleasing she led met. Estate was tended ten boy nearer seemed. As so
+          seeing latter he should thirty whence. Steepest speaking up attended it as. Made neat an
+          on be gave show snug tore.
+        </div>
+
+        <p class="pb-6">
+          Exquisite cordially mr happiness of neglected distrusts. Boisterous impossible unaffected
+          he me everything. Is fine loud deal an rent open give. Find upon and sent spot song son
+          eyes. Do endeavor he differed carriage is learning my graceful. Feel plan know is he like
+          on pure. See burst found sir met think hopes are marry among. Delightful remarkably new
+          assistance saw literature mrs favourable.
+        </p>
+
+        <h2 class="mb-4 mt-4 text-2xl font-semibold text-gray-800">
+          Uneasy barton seeing remark happen his has
+        </h2>
+
+        <p class="pb-6">
+          Guest it he tears aware as. Make my no cold of need. He been past in by my hard. Warmly
+          thrown oh he common future. Otherwise concealed favourite frankness on be at dashwoods
+          defective at. Sympathize interested simplicity at do projecting increasing terminated. As
+          edward settle limits at in.
+        </p>
+
+        <p class="pb-6">
+          Dashwood contempt on mr unlocked resolved provided of of. Stanhill wondered it it welcomed
+          oh. Hundred no prudent he however smiling at an offence. If earnestly extremity he he
+          propriety something admitting convinced ye. Pleasant in to although as if differed
+          horrible. Mirth his quick its set front enjoy hoped had there. Who connection imprudence
+          middletons too but increasing celebrated principles joy. Herself too improve gay winding
+          ask expense are compact. New all paid few hard pure she.
+        </p>
+
+        <p class="pb-6">
+          Breakfast agreeable incommode departure it an. By ignorant at on wondered relation. Enough
+          at tastes really so cousin am of. Extensive therefore supported by extremity of contented.
+          Is pursuit compact demesne invited elderly be. View him she roof tell her case has sigh.
+          Moreover is possible he admitted sociable concerns. By in cold no less been sent hard
+          hill.
+        </p>
+
+        <p class="pb-6">
+          Detract yet delight written farther his general. If in so bred at dare rose lose good.
+          Feel and make two real miss use easy. Celebrated delightful an especially increasing
+          instrument am. Indulgence contrasted sufficient to unpleasant in in insensible favourable.
+          Latter remark hunted enough vulgar say man. Sitting hearted on it without me.
+        </p>
+      </div>
+    </main>
+    <!-- main ends here -->
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
--- oto-galery/src/views/CarDetailsView.vue
+++ oto-galery/src/views/CarDetailsView.vue
@@ -1,7 +1,20 @@
+<script setup>
+import Cardata from '../data/carData.json';
+const carData = Cardata;
+</script>
 <template>
   <section>
     <div class="grid w-full grid-cols-12 place-content-center gap-4 px-4 md:pl-8 md:pr-0">
-      <div class="col-span-12 h-96 bg-black/10 md:col-span-8"></div>
+      <div class="col-span-12 flex h-96 flex-wrap gap-2 bg-black/10 md:col-span-8">
+        <div class="w-1/2">image carousel</div>
+        <div class="w-5/12">
+          <ul>
+            <li v-for="car in carData.cars" :key="car.id">{{ car }}</li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- iletişim -->
       <div class="hidden bg-green-500/10 px-4 py-4 text-center md:col-span-3 md:block">
         <h2 class="pb-4 font-bold">İletişim</h2>
         <ul class="px-3 pb-4 text-left">
@@ -22,7 +35,8 @@
             href="https://yandex.com.tr/harita/101474/konya/?ll=32.492129%2C37.874904&utm_medium=mapframe&utm_source=maps&z=12"
             style="color: #eee; font-size: 12px; position: absolute; top: 14px"
             >Yandex Maps: Ulaşım, navigasyon, yer arama</a
-          ><iframe
+          >
+          <iframe
             src="https://yandex.com.tr/map-widget/v1/?ll=32.492129%2C37.874904&z=12"
             class="aspect-square w-full"
             frameborder="1"
