e1ff786399da5b88074dbe06b8321b2f321e3e71
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-04-29 17:41:23 +0300

    commit

---
+++ oto-galery/src/components/HomeViewContact.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="mt-12 flex w-full flex-col items-center justify-center py-4">
+    <h2 class="pb-4 text-2xl font-bold">Harita ve Adres Bilgisi</h2>
+    <div class="flex w-full flex-wrap justify-center gap-2">
+      <!-- adres falan -->
+
+      <ul class="w-1/3 px-4 py-4">
+        <li class="pb-2"><span class="font-bold">Telefon:</span> 0545 323 73 68</li>
+        <li class="pb-2"><span class="font-bold">WhatsApp:</span> 0545 323 73 68</li>
+        <li class="pb-2"><span class="font-bold">E-posta:</span> ozbeylerauto@gmail.com</li>
+        <li class="pb-2">
+          <span class="font-bold">Adres:</span> bilmemne mh. sanayi sitesi no:15 özbeyler auto
+          çumra/konya
+        </li>
+      </ul>
+
+      <!-- harita bilgisi -->
+      <div class="relative w-1/3 overflow-hidden">
+        <a
+          href="https://yandex.com.tr/harita/101474/konya/?utm_medium=mapframe&utm_source=maps"
+          style="color: #eee; font-size: 12px; position: absolute; top: 0px"
+          >Konya</a
+        ><a
+          href="https://yandex.com.tr/harita/101474/konya/?ll=32.492129%2C37.874904&utm_medium=mapframe&utm_source=maps&z=12"
+          style="color: #eee; font-size: 12px; position: absolute; top: 14px"
+          >Yandex Maps: Ulaşım, navigasyon, yer arama</a
+        >
+        <iframe
+          src="https://yandex.com.tr/map-widget/v1/?ll=32.492129%2C37.874904&z=12"
+          class="aspect-square w-full"
+          frameborder="1"
+          allowfullscreen="true"
+          style="position: relative"
+        ></iframe>
+      </div>
+    </div>
+
+    <!-- maps end -->
+  </section>
+</template>
--- oto-galery/src/views/HomeView.vue
+++ oto-galery/src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import HeroSection from '../components/HeroSection.vue';
 import ProductsSection from '../components/ProductsSection.vue';
+import HomeViewContact from '../components/HomeViewContact.vue';
 </script>
 <template>
   <main>
     <HeroSection />
     <ProductsSection />
+    <HomeViewContact />
   </main>
 </template>
