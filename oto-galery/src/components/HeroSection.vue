f390e594807bf23372cbe6cf372f40e96cf4aa36
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-05-05 17:30:08 +0300

    add width to hero image

--- oto-galery/src/components/HeroSection.vue
+++ oto-galery/src/components/HeroSection.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="flex flex-wrap justify-between gap-4 px-2 sm:px-8">
     <div class="relative flex w-full items-center justify-center">
-      <img src="../assets/hero-img.png" class="" alt="" />
+      <img src="../assets/hero-img.png" class="w-full" alt="hero image" />
     </div>
   </section>
 </template>
